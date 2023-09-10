b29b6ff4c30d5756cae508a3ab8cd4810902bc90
Sky_Thunder <107789489+Skys-Thunder@users.noreply.github.com>
2023-09-10 23:54:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function ok(){
+    z="";
+    try{
     z=CryptoJS.AES.decrypt("U2FsdGVkX1+27XpGh9JVMQUPTttcEDwecPjUz9obImqPoi7ET/R+2HPYBP8LBFow", document.getElementById("pass").value).toString(CryptoJS.enc.Utf8);
+
+    }
+    catch(e){
+        
+    }
     document.getElementById("return").innerText=(z=="")?"パスワードが違います。":z;
 }
